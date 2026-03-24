dcf429e4706b8a06a7602db44fd3b36e8215aaec
Ruben Mesquita <75766940+rubenmesquita87@users.noreply.github.com>
2026-03-24 17:51:55 -0300

    Atualizando novamente as rotas

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,10 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthLoginDto } from './dto/auth-login.dto';
 import { AuthRegisterDto } from './dto/auth-register.dto';
 import { AuthForgetDto } from './dto/auth-forget.dto';
 import { AuthResetDto } from './dto/auth-reset.dto';
 import { AuthService } from './auth.service';
 import { UsuariosService } from 'src/usuarios/usuarios.service';
-import { JwtGuard } from './jwt.guard';
-import { AuthUser } from './auth-user.decorator';
-import { Public } from './auth-public.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -17,32 +14,27 @@ export class AuthController {
   ) {}
 
   @Post('login')
-  @Public()
   async login(@Body() { email, senha }: AuthLoginDto) {
     return await this.authService.login(email, senha);
   }
 
   @Post('register')
-  @Public()
   async register(@Body() body: AuthRegisterDto) {
     return await this.authService.register(body);
   }
 
   @Post('forget')
-  @Public()
   async forget(@Body() { email }: AuthForgetDto) {
     return await this.authService.forget(email);
   }
 
   @Post('reset')
-  @Public()
   async reset(@Body() { senha, token }: AuthResetDto) {
     return await this.authService.reset(senha, token);
   }
 
   @Get('me')
-  @UseGuards(JwtGuard)
-  async me(@AuthUser() user: any) {
-    return user;
+  async me() {
+    return await this.usuariosService.findAll();
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,13 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsuariosModule } from 'src/usuarios/usuarios.module';
 import { Usuarios } from 'src/database/entities/usuarios.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
-    PassportModule,
     JwtModule.register({
       secret: String(process.env.JWT_SECRET),
       signOptions: { expiresIn: '7 days' },
@@ -19,7 +16,7 @@ import { JwtStrategy } from './jwt.strategy';
     UsuariosModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService],
   exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
